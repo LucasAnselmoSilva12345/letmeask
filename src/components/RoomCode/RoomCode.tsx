7202bea5eee4410a6ba6575a6d307a4034f71923
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2021-06-27 19:30:12 -0300

    Atualização

--- src/components/RoomCode/RoomCode.tsx
+++ src/components/RoomCode/RoomCode.tsx
@@ -1,24 +1,38 @@
-import copyImg from '../../assets/images/copy.svg'
+import toast, { Toaster } from 'react-hot-toast';
 
-import './room-code.css'
+import copyImg from '../../assets/images/copy.svg';
+
+import './room-code.css';
 
 type RoomCodeProps = {
   code: string;
-}
+};
 
-export function RoomCode(props: RoomCodeProps){
+export function RoomCode(props: RoomCodeProps) {
 
-  function copyRoomCodeToClipBoard(){
-    navigator.clipboard.writeText(props.code)
-    alert('Código copiado')
-  }
+    const notify = () => toast.error('Código Copiado', {
+      style: {
+        border: '1px solid #12990d',
+        padding: '16px',
+        color: '#12990d'
+      },
+      iconTheme: {
+        primary: '#12990d',
+        secondary: '#fffaee',
+      },
+    })
+    navigator.clipboard.writeText(props.code);
 
-  return(
-    <button className="room-code" onClick={copyRoomCodeToClipBoard}>
-      <div>
-        <img src={copyImg} alt="Copy run code" />
-      </div>
-      <span>Sala {props.code}</span>
-    </button>
-  )
+
+  return (
+    <>
+      <button className="room-code" onClick={notify}>
+        <div>
+          <img src={copyImg} alt="Copy run code" />
+        </div>
+        <span>Sala {props.code}</span>
+      </button>
+      <Toaster />
+    </>
+  );
 }
