632480010522bdc96dce7a187d0f6363437edceb
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2021-06-27 19:30:42 -0300

    Page admin

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,5 +1,6 @@
 import { useHistory, useParams } from 'react-router-dom';
 
+
 import logoImg from '../../assets/images/logo.svg';
 import deleteImg from '../../assets/images/delete.svg';
 import checkImg from '../../assets/images/check.svg';
